dcd75d89e1a4a2c2611f4eb497c20eeaee8e5c6c
kingslyM <82265186+kingslyM@users.noreply.github.com>
2021-04-10 16:18:02 +0530

    Create circleArea.py
    Finding the area of a circle

---
+++ circleArea.py
@@ -0,0 +1,6 @@
+#Finding the area of a circle with variable radius
+radius = input("Enter radius of the circle\n")
+r = float(radius)
+pi = 3.1415926
+circleArea = pi * r * r
+print("Area of the circle with radius ",r," = ",circleArea )
